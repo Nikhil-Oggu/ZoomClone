37519a28b25734b8831e3f6e91c9cb61aa4e5557
Nikhil Oggu <oggunikhil174@gmail.com>
2026-01-04 19:34:34 +0530

    implement socket connection and user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,39 @@
-import express from 'express';
-import {createServer} from 'node:http';
-import {Server} from 'socket.io';
-import mongoose from 'mongoose';    
-import cors from 'cors';
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
-app.get('/home', (req, res) => {
-   return res.json({'Hello': 'World!'});
-});
+const server = createServer(app);
+const io = connectToSocket(server);
 
-const start=async () => {
-   app.listen(8000, () => {
-      console.log('Server is running on port 8000');
-   });}
 
-   
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://nikhiloggu_db_user:iMkpxbVqUWnDtxyx@zerodhacluster.x4xuo9r.mongodb.net/zoom")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
+
+
+
 start();
